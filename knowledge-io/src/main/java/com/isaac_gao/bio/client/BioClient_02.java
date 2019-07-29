8d529dca84ae1dd164bd38122dd25497f5399e65
IsaacGao <Gzy3881@vip.qq.com>
2019-07-30 00:00:43 +0800

    * add bio client.

---
+++ knowledge-io/src/main/java/com/isaac_gao/bio/client/BioClient_02.java
@@ -0,0 +1,31 @@
+package com.isaac_gao.bio.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+@SuppressWarnings("ALL")
+public class BioClient_02 {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 9956);
+            if (socket.isConnected()) {
+                System.out.println("连接客户端成功.");
+                OutputStream outputStream = socket.getOutputStream();
+                System.out.print("请输入：");
+                Scanner scanner = new Scanner(System.in);
+                String msg = scanner.nextLine();
+                System.out.println("msg:" + msg);
+                //!!!! 这个地方是阻塞的，只有写出成功才会继续
+                outputStream.write(msg.getBytes());
+                System.out.println("写出完成");
+                scanner.close();
+                outputStream.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
